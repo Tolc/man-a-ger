b038684e713c25160cccc3746cfe794e8c853dcb
Tolc <thomas.coquel117@gmail.com>
2015-02-09 17:56:55 +0100

    stats module refactored

--- public/modules/stats/config/stats.client.routes.js
+++ public/modules/stats/config/stats.client.routes.js
@@ -5,21 +5,9 @@ angular.module('stats').config(['$stateProvider',
 	function($stateProvider) {
 		// Stats state routing
 		$stateProvider.
-		state('listStats', {
+		state('stats', {
 			url: '/stats',
-			templateUrl: 'modules/stats/views/list-stats.client.view.html'
-		}).
-		state('createStat', {
-			url: '/stats/create',
-			templateUrl: 'modules/stats/views/create-stat.client.view.html'
-		}).
-		state('viewStat', {
-			url: '/stats/:statId',
-			templateUrl: 'modules/stats/views/view-stat.client.view.html'
-		}).
-		state('editStat', {
-			url: '/stats/:statId/edit',
-			templateUrl: 'modules/stats/views/edit-stat.client.view.html'
+			templateUrl: 'modules/stats/views/stats.client.view.html'
 		});
 	}
 ]);
